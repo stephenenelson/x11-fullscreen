34d49a30846ffba925b720105c500328f2f47943
Stephen Nelson <stephenenelson@mac.com>
2013-08-22 00:42:39 -0700

    Moved custom code to PkgConfBuild

---
+++ inc/PkgConfBuild.pm
@@ -0,0 +1,28 @@
+package PkgConfBuild;
+
+use base 'Module::Build';
+
+use strict;
+use warnings;
+
+use Devel::CheckLib;
+use ExtUtils::PkgConfig;
+
+sub new {
+	my $type = shift;
+	my %opts = @_;
+	
+	check_lib_or_exit(
+	    lib => [qw(Imlib2 X11)]
+    );
+    
+	my %x11_info = ExtUtils::PkgConfig->find('x11');
+	my %imlib2_info = ExtUtils::PkgConfig->find('imlib2');
+
+    $opts{'extra_linker_flags'} = $imlib2_info{'libs'} . ' ' . $x11_info{'libs'};
+    $opts{'extra_compiler_flags'} = '-I. ' . $imlib2_info{'cflags'} . ' ' . $x11_info{'cflags'};
+
+	return $type->SUPER::new(%opts);
+}
+
+1;
